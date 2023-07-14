02181b35352622342954f7ea80a261ff178986d1
weneedFOOD <weneedfood.jla@gmail.com>
2023-07-14 15:49:29 -0500

    Update index.jsx
    feat!: updated code to render MainView component.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 import { createRoot } from 'react-dom/client';
-
+import { MainView } from './components/main-view/main-view';
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <div>Good morning</div>
-    </div>
-  );
+export const App = () => {
+  return <MainView />;
 };
 
 // Finds the root of your app
@@ -17,4 +13,4 @@ const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<App />);
