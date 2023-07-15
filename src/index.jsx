0f7afc69af927b866dbaa6d010991ba7cda9962b
weneedFOOD <weneedfood.jla@gmail.com>
2023-07-15 16:14:27 -0500

    Update index.jsx
    chore!: removed export

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { MainView } from './components/main-view/main-view';
 import "./index.scss";
 
 // Main component (will eventually use all the others)
-export const App = () => {
+const App = () => {
   return <MainView />;
 };
 
