b91ce50a9acdd5c877b5f79df6281d3768635cf8
weneedFOOD <weneedfood.jla@gmail.com>
2023-07-19 00:29:40 -0500

    Update index.jsx
    chore!: change single quotes to double quotes

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import { MainView } from './components/main-view/main-view';
+import { createRoot } from "react-dom/client";
+import { MainView } from "./components/main-view/main-view";
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
-const App = () => {
+export const App = () => {
   return <MainView />;
 };
 
