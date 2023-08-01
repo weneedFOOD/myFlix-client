2d232323b55bc58daef1bd7b0a71de4fe4b9b3e7
weneedFOOD <weneedfood.jla@gmail.com>
2023-08-01 01:12:59 -0500

    Update movie-view.jsx
    feat! added objects Description, ReleaseYear, Genre to movie and Name, Bio, Birth to Director.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,16 +1,38 @@
 export const MovieView = ({ movie, onBackClick }) => {
+    const {ImagePath, Title, Description, ReleaseYear, Genre, Director} = movie;
+    const {Name, Bio, Birth} = Director;
     return (
         <div>
             <div>
-                <img src={movie.ImagePath} alt={movie.Title}/>
+                <img src={ImagePath}/>
             </div>
             <div>
                 <span>Title: </span>
-                <span>{movie.Title}</span>
+                <span>{Title}</span>
+            </div>
+            <div>
+                <span>Description: </span>
+                <span>{Description}</span>
+            </div>
+            <div>
+                <span>ReleaseYear: </span>
+                <span>{ReleaseYear}</span>
+            </div>
+            <div>
+                <span>Genre: </span>
+                <span>{Genre}</span>
             </div>
             <div>
                 <span>Director: </span>
-                <span>{movie.Director}</span>
+                <span>{Name}</span>
+            </div>
+            <div>
+                <span>Bio: </span>
+                <span>{Bio}</span>
+            </div>
+            <div>
+                <span>Birth: </span>
+                <span>{Birth}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
