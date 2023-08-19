9d893bd63b0d7c0006674438e29bcdd035ffe803
weneedFOOD <weneedfood.jla@gmail.com>
2023-08-18 23:34:07 -0500

    Update index.jsx
    feat: added Container to MainView

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view.jsx";
-// Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
+import Container from "react-bootstrap/Container" 
+import './index.scss';
 
 // Main component (will eventually use all the others)
-export const App = () => {
-  return <MainView />;
+const App = () => {
+  return (
+    <Container fluid>
+      <MainView />
+    </Container>);
 };
 
 // Finds the root of your app
