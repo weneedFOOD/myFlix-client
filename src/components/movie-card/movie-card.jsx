d7d37a3b155fe284aa3aa2722ee1ae0ec5c63da6
weneedFOOD <weneedfood.jla@gmail.com>
2023-09-15 23:03:19 -0500

    Update movie-card.jsx
    feat!: added feat to add or remove Favorite Movie in MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,23 +1,99 @@
 // Here you import the PropTypes library
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Card } from "react-bootstrap";
-import { Link, Button } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./movie-card.css";
 
 // The MovieCard function component
-export const MovieCard = ({ movie, onMovieClick}) => {
+export const MovieCard = ({ movie, user, token, setUser }) => {
+
+  const [isFavorite, setIsFavorite] = useState (
+    user.addFavoriteMovie.includes(movie._id)
+  )
+    
+  const addFavoriteMovie = () => {
+    fetch(
+      `https://hora-flix-f4f11200119c.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+      {
+        method: 'POST',
+        headers: { Authorization: `Bearer ${token}` }
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert('Failed');
+          return false;
+        }
+      })
+      .then((user) => {
+        if (user) {
+          alert('successfully added to favorites');
+          localStorage.setItem("user", JSON.stringify(user)); // updating user on local storage
+          setUser(user); // updating the react application
+          setIsFavorite(true);
+        }
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  };
+
+  const removeFavoriteMovie = () => {
+    fetch(
+      `https://hora-flix-f4f11200119c.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+      {
+        method: "DELETE",
+        headers: { Authorization: `Bearer ${token}` }
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Failed");
+          return false;
+        }
+      })
+      .then((user) => {
+        if (user) {
+          alert("Successfully deleted from favorites");
+          localStorage.setItem("user", JSON.stringify(user)); // updating user on local storage
+          setUser(user); // updating the react application
+          setIsFavorite(false);
+        }
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  };
+
     return (
-        <Card>
+      <>
+        <Card className="h-100">
             <Card.Img variant="top" src={movie.ImagePath} />
             <Card.Body>
-               <Card.Title>{movie.Title}</Card.Title>
+              {isFavorite ? (
+                <Button variant="Danger" onClick={removeFavoriteMovie}>
+                  Remove from favorites
+                </Button>
+              ) : (
+                <Button variant="primary" onClick={addFavoriteMovie}>
+                  Add to favorites
+                </Button>
+              )}
+            </Card.Body>
+
+            <Card.Body>
                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-                <Button variant="link">Open</Button>
+                <Button className="info-button" variant="outline-light">Info</Button>
                </Link>
              </Card.Body>
-         </Card>  
+         </Card>
+      </>    
     );
 };
 
