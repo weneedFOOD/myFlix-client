061a888b3fa9b73813e3259a1075366cfba1f738
weneedFOOD <weneedfood.jla@gmail.com>
2023-09-24 01:06:10 -0500

    Update navigation-bar.jsx
    feat: changed Navbar.Brand to Hora-Flix

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -6,7 +6,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          Movies App
+          Hora-Flix
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
